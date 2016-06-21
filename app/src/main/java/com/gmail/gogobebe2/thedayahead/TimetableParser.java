26eafa3f98f018b40342371c5e4ac098be330f48
gogobebe2 <gogobebe2@gmail.com>
2016-06-21 21:55:49 +1200

    TimetableParser skeleton

--- app/src/main/java/com/gmail/gogobebe2/thedayahead/TimetableParser.java
+++ app/src/main/java/com/gmail/gogobebe2/thedayahead/TimetableParser.java
@@ -2,4 +2,26 @@ package com.gmail.gogobebe2.thedayahead;
 
 public class TimetableParser {
     // TODO make this class save information about the timetable.
+    private Day[] days = new Day[5];
+    private final String HTML;
+
+    TimetableParser(String html) {
+        this.HTML = html;
+    }
+
+    private class Day {
+        private Period[] periods;
+        private String date;
+
+        private Day(Period[] periods, String date) {
+            this.periods = periods;
+            this.date = date;
+        }
+    }
+
+    private class Period {
+        private String subjectName;
+        private String teacherInitials;
+        private String classroom;
+    }
 }
