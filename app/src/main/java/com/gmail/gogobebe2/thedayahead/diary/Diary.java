893d237eff5e01d0085440d5e695dcaf20d77989
gogobebe2 <gogobebe2@gmail.com>
2016-08-23 13:00:22 +1200

    asd

--- app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/Diary.java
+++ app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/Diary.java
@@ -1,4 +1,31 @@
 package com.gmail.gogobebe2.thedayahead.diary;
 
 public class Diary {
+    private String text;
+    private int day;
+    private int hour;
+    private int minute;
+
+    Diary(String text, int day, int hour, int minute) {
+        this.text = text;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
--- app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/DiaryFragment.java
+++ app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/DiaryFragment.java
@@ -6,24 +6,41 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.gmail.gogobebe2.thedayahead.R;
 import com.gmail.gogobebe2.thedayahead.TheDayAheadFragment;
 
-public class DiaryFragment extends TheDayAheadFragment {
+public class DiaryFragment extends TheDayAheadFragment implements View.OnClickListener {
     public DiaryFragment() {/* Required empty public constructor*/}
+
+    private LinearLayout linearLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false);
+        linearLayout = (LinearLayout) inflater.inflate(R.layout.fragment_diary, container, false);;
+        return linearLayout;
     }
 
     @NonNull
     @Override
     protected String getTitle() {
         return "Diary";
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_saveAndSetDateAndTimeDiary) {
+            EditText editText = (EditText) linearLayout.findViewById(R.id.editText_diary);
+            String text = editText.getText().toString();
+            int dayInt = -1;
+            int hourInt = -1;
+            int minuteInt = -1;
+            Diary diary = new Diary(text, dayInt,hourInt, minuteInt);
+        }
     }
 }
