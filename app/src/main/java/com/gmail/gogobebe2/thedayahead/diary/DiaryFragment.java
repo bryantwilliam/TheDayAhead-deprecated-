7094071ae135c7b752cb26219040528d84aedf25
gogobebe2 <gogobebe2@gmail.com>
2016-09-04 11:35:33 +1200

    got periodlist showing periods.

--- app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/DiaryFragment.java
+++ app/src/main/java/com/gmail/gogobebe2/thedayahead/diary/DiaryFragment.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
@@ -15,6 +17,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -23,11 +26,14 @@ import com.gmail.gogobebe2.thedayahead.R;
 import com.gmail.gogobebe2.thedayahead.TheDayAheadFragment;
 import com.gmail.gogobebe2.thedayahead.timetable.SubjectType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiaryFragment extends TheDayAheadFragment implements View.OnClickListener,
-        CompoundButton.OnCheckedChangeListener {
+        CompoundButton.OnCheckedChangeListener, AdapterView.OnItemClickListener {
     public DiaryFragment() {/* Required empty public constructor*/}
 
-    private LinearLayout fragmentDiary;
+    private LinearLayout fragmentDiaryXML;
     private LinearLayout diaryDialogByDateAndTime;
     private ListView listViewPeriodSelector;
     private ListView listViewDiaryEntries;
@@ -37,8 +43,8 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        fragmentDiary = (LinearLayout) inflater.inflate(R.layout.fragment_diary, parent, false);
-        listViewDiaryEntries = (ListView) fragmentDiary.findViewById(R.id.listView_diaryEntries);
+        fragmentDiaryXML = (LinearLayout) inflater.inflate(R.layout.fragment_diary, parent, false);
+        listViewDiaryEntries = (ListView) fragmentDiaryXML.findViewById(R.id.listView_diaryEntries);
 
         diaryDialog = new Dialog(getContext());
         diaryDialog.setContentView(R.layout.fragment_diary_dialog);
@@ -46,13 +52,13 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
         diaryDialogByDateAndTime = (LinearLayout) diaryDialog.findViewById(R.id.linearlayout_dialogByDateAndTime);
         listViewPeriodSelector = (ListView) diaryDialog.findViewById(R.id.listView_periodSelector);
 
-        ((Switch) fragmentDiary.findViewById(R.id.switch_dateTimeOrPeriod)).setOnCheckedChangeListener(this);
-        fragmentDiary.findViewById(R.id.button_pickDateOrPeriod).setOnClickListener(this);
+        ((Switch) fragmentDiaryXML.findViewById(R.id.switch_dateTimeOrPeriod)).setOnCheckedChangeListener(this);
+        fragmentDiaryXML.findViewById(R.id.button_pickDateOrPeriod).setOnClickListener(this);
         diaryDialogByDateAndTime.findViewById(R.id.button_doneDateAndTimeSelection).setOnClickListener(this);
         diaryDialogByDateAndTime.findViewById(R.id.button_pickDate).setOnClickListener(this);
         diaryDialogByDateAndTime.findViewById(R.id.button_pickTime).setOnClickListener(this);
 
-        return fragmentDiary;
+        return fragmentDiaryXML;
     }
 
     @NonNull
@@ -65,7 +71,7 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
     public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
         final Switch switchView = (Switch) buttonView;
 
-        final Button button = (Button) fragmentDiary.findViewById(R.id.button_pickDateOrPeriod);
+        final Button button = (Button) fragmentDiaryXML.findViewById(R.id.button_pickDateOrPeriod);
 
         if (isChecked) {
             if (MainActivity.timetable == null) {
@@ -80,17 +86,23 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
         String buttonTitle = "Save entry and pick ";
 
         if (isChecked) {
-            for (final SubjectType subjectType : MainActivity.timetable.getSubjects()) {
-                Button subjectButton = new Button(getContext());
-                subjectButton.setText(subjectType.getName());
-                subjectButton.setOnClickListener(this);
-                listViewPeriodSelector.addHeaderView(subjectButton);
-            }
+
+            List<String> subjectNames = new ArrayList<>();
+            for (final SubjectType subjectType : MainActivity.timetable.getSubjects())
+                subjectNames
+                        .add(subjectType.getName());
+
+            listViewPeriodSelector.setAdapter(new ArrayAdapter<>(getContext(),
+                    R.layout.fragment_diary_dialog_byperiod_listitem,
+                    subjectNames.toArray(new String[subjectNames.size()])));
+
+            listViewPeriodSelector.setOnItemClickListener(this);
 
             listViewPeriodSelector.setVisibility(View.VISIBLE);
             diaryDialogByDateAndTime.setVisibility(View.GONE);
 
             buttonTitle += "period";
+
 
         } else {
             listViewPeriodSelector.setVisibility(View.GONE);
@@ -101,8 +113,30 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
 
         final String finalButtonTitle = buttonTitle;
         button.setText(finalButtonTitle);
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (view instanceof TextView) {
+            TextView textView = (TextView) view;
+            for (SubjectType subjectType : MainActivity.timetable.getSubjects()) {
+                String subjectName = subjectType.getName();
+                if (textView.getText().equals(subjectName)) {
+                    int firstPeriodIndex = subjectType.getFirstPeriodIndex();
 
+                    int dayInt = subjectType.getFirstDayIndex();
+                    int hourInt = MainActivity.timetable.getHour(firstPeriodIndex);
+                    int minuteInt = MainActivity.timetable.getMinute(firstPeriodIndex);
+
+                    EditText editText = (EditText) fragmentDiaryXML.findViewById(R.id.editText_diary);
+
+                    DiaryEntryMakerAndSaver.makeAndSaveDiaryEntry(editText, dayInt, hourInt
+                            , minuteInt, getContext(), listViewDiaryEntries);
+
+                    diaryDialog.dismiss();
+                }
+            }
+        }
     }
 
     @Override
@@ -110,14 +144,9 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
         if (view instanceof Button) {
             Button button = (Button) view;
 
-            int dayInt;
-            int hourInt;
-            int minuteInt;
-            int monthInt;
-
-            EditText editText = (EditText) button.findViewById(R.id.editText_diary);
             TimePicker timePicker = (TimePicker) button.findViewById(R.id.timePicker);
             DatePicker datePicker = (DatePicker) button.findViewById(R.id.datePicker);
+
             LinearLayout linearLayoutDialogByDateAndTime = (LinearLayout) button.findViewById(
                     R.id.linearlayout_dialogByDateAndTime);
 
@@ -128,11 +157,14 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
                     diaryDialog.show();
                     return; // *
                 case R.id.button_doneDateAndTimeSelection:
+
+
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        monthInt = datePicker.getMonth();
-                        dayInt = datePicker.getDayOfMonth();
-                        hourInt = timePicker.getHour();
-                        minuteInt = timePicker.getMinute();
+                        int dayInt = datePicker.getDayOfMonth();
+                        int hourInt = timePicker.getHour();
+                        int minuteInt = timePicker.getMinute();
+                        int monthInt = datePicker.getMonth();
+                        EditText editText = (EditText) button.findViewById(R.id.editText_diary);
 
                         DiaryEntryMakerAndSaver.makeAndSaveDiaryEntry(editText, monthInt, dayInt,
                                 hourInt, minuteInt, getContext(), listViewDiaryEntries);
@@ -159,21 +191,7 @@ public class DiaryFragment extends TheDayAheadFragment implements View.OnClickLi
                     return; // *
             }
 
-            for (SubjectType subjectType : MainActivity.timetable.getSubjects()) {
-                String subjectName = subjectType.getName();
-                if (button.getText().equals(subjectName)) {
-                    int firstPeriodIndex = subjectType.getFirstPeriodIndex();
 
-                    dayInt = subjectType.getFirstDayIndex();
-                    hourInt = MainActivity.timetable.getHour(firstPeriodIndex);
-                    minuteInt = MainActivity.timetable.getMinute(firstPeriodIndex);
-
-                    DiaryEntryMakerAndSaver.makeAndSaveDiaryEntry(editText, dayInt, hourInt
-                            , minuteInt, getContext(), listViewDiaryEntries);
-
-                    diaryDialog.dismiss();
-                }
-            }
         }
     }
 
